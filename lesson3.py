ade377ac1413cb667d14d942ddf58c5f5e40271f
Krobirow <bodia122@gmail.com>
2021-05-21 17:14:51 +0200

    lesson 3 homework upd

---
+++ lesson3.py
@@ -0,0 +1,26 @@
+# Задание 1
+
+# три команды
+print("G", "l", "o",  sep="", end=" ")
+print("Aca", "de", sep="", end="")
+print("my")
+
+print()
+# одна команда
+print("Glo Academy")
+
+# Задание 2
+print("_____________")
+print("Как тебя зовут?")
+name = input()
+print("Привет", name, sep=", ")
+
+# Задание 3
+print("_____________")
+print("Как вас зовут?")
+name = input()
+print("Какая у вас фамилия?")
+surname = input()
+print("Какое у вас отчество?")
+father_name = input()
+print(name, surname, father_name, sep="-")
