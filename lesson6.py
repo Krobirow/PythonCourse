a1da3141e1bcb13b811e0a45bec876066c87c0e9
krobirow <krobirow@gmail.com>
2021-05-29 10:34:04 +0200

    lesson6 fix

--- lesson6.py
+++ lesson6.py
@@ -22,7 +22,7 @@ print(ex4_str1 * 5)
 # exercise 5
 ex5_str1 = input()
 
-if "@" and "." in ex5_str1:
+if "@" in ex5_str1 and "." in ex5_str1:
     print("Корректный")
 else:
     print("Некорректный")
